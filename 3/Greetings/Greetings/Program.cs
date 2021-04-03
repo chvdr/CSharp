376a735cfd987f3eccc5070eb49753fe11270e05
chvdr <chvdr@abv.bg>
2021-04-04 01:13:14 +0300

    solve homework from 3/last task only left

---
+++ 3/Greetings/Greetings/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Greetings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + "!");
+        }
+    }
+}
---
+++ 3/square/square/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace square
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int side = int.Parse(Console.ReadLine());
+            int area = side * side;
+            Console.WriteLine(area); 
+        }
+    }
+}
