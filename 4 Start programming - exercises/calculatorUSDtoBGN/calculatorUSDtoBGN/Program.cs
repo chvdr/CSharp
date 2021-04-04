5a1e14a5ff9ba4e196a392ec1d6fa2a01a04892a
chvdr <chvdr@abv.bg>
2021-04-04 11:03:48 +0300

    First Steps In Coding - Exercise - tasks 1 and 2

---
+++ 4 Start programming - exercises/calculatorUSDtoBGN/calculatorUSDtoBGN/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace calculatorUSDtoBGN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("USD: "); double usd = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Confirmation: " + usd + " USD are accepted. ");
+            double bgn = usd * 1.79549;
+
+            Console.WriteLine("BGN: " + bgn);
+        }
+    }
+}
---
+++ 4 Start programming - exercises/converterRADtoDEG/converterRADtoDEG/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace converterRADtoDEG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Please enter the angle in RADIANS: "); 
+            double radians = double.Parse(Console.ReadLine());
+            double degrees = radians * 180 / Math.PI;
+
+            // Console.WriteLine("Value (DEG): " + Math.Round(degrees));
+            Console.WriteLine(Math.Round(degrees));
+        }
+    }
+}
