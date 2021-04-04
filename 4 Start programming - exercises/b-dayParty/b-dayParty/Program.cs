3a4fcd503bd2acf95956b4e7ceb5bce807130eed
chvdr <chvdr@abv.bg>
2021-04-04 16:26:03 +0300

    "B-Day Party" and "Vacation Book List"

---
+++ 4 Start programming - exercises/b-dayParty/b-dayParty/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace b_dayParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input
+            Console.Write("Please enter the rent of the room: "); double rent = double.Parse(Console.ReadLine());
+
+            double birthdayCace = rent / 5;
+            double drinks = 0.55 * birthdayCace;
+            double animator = rent / 3;
+
+            // Console.WriteLine("Total price: " + (rent + birthdayCace + drinks + animator));
+            Console.WriteLine(rent + birthdayCace + drinks + animator);
+        }
+    }
+}
---
+++ 4 Start programming - exercises/vacationBookList/vacationBookList/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace vacationBookList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // vacationBookList
+            // prints how many hours Joro will read every day to complete the current book in time 
+
+            // vacationBookList variables table/list
+            int pages;      // number of pages in the book Joro currently reading 
+            double pph;        // pages per hour - Joro's reading speed ability 
+            int days;       // number of days the book should be finished
+
+            // input 
+            Console.Write("pages of the book: "); pages = int.Parse(Console.ReadLine());
+            Console.Write("reading speed (pages per hour): "); pph = int.Parse(Console.ReadLine());
+            Console.Write("Days: "); days = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(pages / pph / days);
+        }
+    }
+}
