9f12efd8c6c7d403fdd36296f28aeca1e6457bfe
chvdr <chvdr@abv.bg>
2021-04-04 16:48:58 +0300

    The task "Charity Campaign"

---
+++ 4 Start programming - exercises/charityCampaign/charityCampaign/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace charityCampaign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            int coockers = int.Parse(Console.ReadLine());
+            int cake = int.Parse(Console.ReadLine());
+            int waffles = int.Parse(Console.ReadLine());
+            int pancake = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(((cake * 45 + waffles * 5.80 + pancake * 3.20) * days * coockers) * 7 / 8);        
+        }
+    }
+}
