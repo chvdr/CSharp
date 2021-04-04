9674f6a1baf05bca23931fdc1c85c85bcb73b91b
chvdr <chvdr@abv.bg>
2021-04-04 11:33:44 +0300

    4/exersise, task 3

---
+++ 4 Start programming - exercises/calculatorDeposit/calculatorDeposit/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace calculatorDeposit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Deposit: "); double deposit = double.Parse(Console.ReadLine());
+            Console.Write("Deposit interval (months): "); int months = int.Parse(Console.ReadLine());
+            Console.Write("Yearly revenue (in %): "); double revenue = double.Parse(Console.ReadLine()) / 100;
+
+            Console.WriteLine(deposit + months * (deposit * revenue)/12); 
+        }
+    }
+}
