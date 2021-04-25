00b6db54dad91a95c479c3b3a741ab12fcf61d29
chvdr <chvdr@abv.bg>
2021-04-25 11:20:42 +0300

    Advanced tests exercises - 1. Cinema

---
+++ 8 test advanced - exercises/1_cinema/1_cinema/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1_cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            int rows = int.Parse(Console.ReadLine());
+            int cols = int.Parse(Console.ReadLine());
+            double income = 0.0; 
+
+            if (type == "Premiere")
+            {
+                income = rows * cols * 12.00;
+            }
+            else if (type == "Normal")
+            {
+                income = rows * cols * 7.50;
+            }
+            else if (type == "Discount")
+            {
+                income = rows * cols * 5.00;
+            }
+
+            Console.Write("{0:f2}", income); Console.WriteLine( " leva"); 
+        }
+    }
+}
---
+++ 8 test advanced - exercises/README.txt
@@ -0,0 +1,11 @@
+
+
+Hi 
+
+Bored, so many repetitive tasks with not real sense - skipping all of them 
+
+Still keeping tasks in todo
+
+Cheers, 
+
+Chavdar
