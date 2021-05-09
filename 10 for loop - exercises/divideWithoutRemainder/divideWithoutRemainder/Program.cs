fd3459ed5d6233d98c0185835347af15152dc233
chvdr <chvdr@abv.bg>
2021-05-09 08:50:07 +0300

    5/9/2021: FOR exercises last four tasks

---
+++ 10 for loop - exercises/divideWithoutRemainder/divideWithoutRemainder/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace divideWithoutRemainder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double p1 = 0.00; double p2 = 0.00; double p3 = 0.00;
+
+            for (int i = 1; i <= n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (num % 2 == 0) { p1++; }
+                if (num % 3 == 0) { p2++; }
+                if (num % 4 == 0) { p3++; }
+            }
+
+            p1 = p1 / n * 100; p2 = p2 / n * 100; p3 = p3 / n * 100;
+
+            Console.Write("{0:f2}", p1); Console.WriteLine("%");
+            Console.Write("{0:f2}", p2); Console.WriteLine("%");
+            Console.Write("{0:f2}", p3); Console.WriteLine("%");
+        }
+    }
+}
---
+++ 10 for loop - exercises/remuneration_task6/remuneration_task6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace remuneration_task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int salary = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                string website = Console.ReadLine();
+
+                if (website == "Facebook") { salary = salary - 150; }
+                else if (website == "Instagram") { salary = salary - 100; }
+                else if (website == "Reddit") { salary = salary - 50; }
+
+                if (salary <= 0) { Console.WriteLine("You have lost your salary."); break; }
+            }
+
+            if (salary > 0) { Console.WriteLine(salary); }
+        }
+    }
+}
