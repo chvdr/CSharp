1e382fd1c88da9a794c202e28f6eea89145aa24c
chvdr <chvdr@abv.bg>
2021-05-09 09:16:12 +0300

    Start working on while loop tasks 5/9/2021

---
+++ __Extras/05. PC Game Shop/pcGameShop_testTask05/pcGameShop_testTask05/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace pcGameShop_testTask05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            
+            double hearthstone = 0;
+            double fornite = 0;
+            double overwatch = 0;
+            double others = 0; 
+            
+            
+            for (int i = 1; i <= n; i++)
+            {
+                string gameName = Console.ReadLine();
+            
+                if (gameName == "Hearthstone")
+                {
+                    hearthstone++;
+                    //hearthstone+=1;
+                }
+                else if (gameName == "Fornite")
+                {
+                    fornite++;
+                    //fornite = fornite + 1;
+            
+                }
+                else if (gameName == "Overwatch")
+                {
+                    overwatch += 1;
+                    //overwatch++;
+            
+                }
+                else
+                {
+                    others++;
+                }
+            }
+            
+            double p1 = hearthstone / n * 100;
+            double p2 = fornite / n * 100;
+            double p3 = overwatch / n * 100;
+            double p4 = others / n * 100;
+            
+            
+            Console.WriteLine($"Hearthstone - {p1:F2}%");
+            Console.WriteLine($"Fornite - {p2:F2}%");
+            Console.WriteLine($"Overwatch - {p3:F2}%");
+            Console.WriteLine($"Others - {p4:F2}%");
+        }
+    }
+}
---
+++ __Extras/README.txt
@@ -0,0 +1,7 @@
+This should contain tasks that are completely apply to the exam preparation
+
+
+[1]. "05. PC Game Shop"
+Link към задача "05. PC Game Shop" в Judge: https://judge.softuni.bg/Contests/Practice/Index/1745#8
+Link към решението: https://pastebin.com/9xXHsM21
+
